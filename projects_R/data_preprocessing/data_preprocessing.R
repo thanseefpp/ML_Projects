90054e9a4d69c01e103011e831e203a48a924895
Thanseef <androtsf@gmail.com>
2022-10-19 11:03:50 +0530

    splitting and feature scaling done using R

--- projects_R/data_preprocessing/data_preprocessing.R
+++ projects_R/data_preprocessing/data_preprocessing.R
@@ -4,23 +4,31 @@
 dataset = read.csv('Data.csv')
 
 # Taking care of missing data.
-#is.na is checking the field is empty or not
-#ave is used to find the average.
-
 dataset$Age = ifelse(is.na(dataset$Age),
                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
                      dataset$Age)
 
 dataset$Salary = ifelse(is.na(dataset$Salary),
-                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
-                     dataset$Salary)
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
 
 #Encoding categorical data
-#factor is used to encode a vector as a factor
 dataset$Country = factor(dataset$Country,
                          levels = c('France','Spain','Germany'),
                          labels = c(1,2,3))
 
 dataset$Purchased = factor(dataset$Purchased,
-                         levels = c('Yes','No'),
-                         labels = c(1,0))
+                           levels = c('Yes','No'),
+                           labels = c(1,0))
+# Splitting the dataset into the training set and test set.
+#install.packages('caTools') #installing package
+library(caTools)
+set.seed(123)
+
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+testing_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[, 2:3] = scale(training_set[, 2:3])
+testing_set[, 2:3] = scale(testing_set[, 2:3])
